a7e7728ce4f76784ff6da57d949d230e5383854b
handikatriarlan <handikaarlan@gmail.com>
2025-04-07 12:11:54 +0700

    feat(product): add create product endpoint

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -6,6 +6,7 @@ use App\Models\Product;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ProductResource;
+use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
@@ -13,6 +14,34 @@ class ProductController extends Controller
     {
         $products = Product::latest()->paginate(5);
 
-        return new ProductResource(true, 'List Data Products', $products);
+        return new ProductResource(true, 'Product Data List', $products);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'image'         => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'title'         => 'required',
+            'description'   => 'required',
+            'price'         => 'required|numeric',
+            'stock'         => 'required|numeric',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $image = $request->file('image');
+        $image->storeAs('products', $image->hashName());
+
+        $product = Product::create([
+            'image'         => $image->hashName(),
+            'title'         => $request->title,
+            'description'   => $request->description,
+            'price'         => $request->price,
+            'stock'         => $request->stock,
+        ]);
+
+        return new ProductResource(true, 'Product Data Successfully Added!', $product);
     }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Casts\Attribute;
 class Product extends Model
 {
     protected $fillable = [
+        'image',
         'title',
         'description',
         'price',
